e31b4fa037664368c371690e16b0028c88dda16e
kumaranish8888 <kumaranish8888@gmail.com>
2017-05-02 16:20:12 -0400

    Added code

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-function greeting(greet){
-    console.log(greet);
-}
-greeting("Hello World");
+const http = require('http');
+
+const hostname = 'localhost';
+
+const port = 8000;
+
+const server = http.createServer((req, res) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server started on port ' +port); 
+});
